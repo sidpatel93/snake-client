fa08ae190c595cbeadec49757502824b6e361541
spatel93 <sid.auburn@gmail.com>
2021-11-02 18:11:23 +0000

    implement key press commands convey to server.

--- input.js
+++ input.js
@@ -5,6 +5,7 @@ let connection;
 const setupInput = function (conn) {
   const stdin = process.stdin;
   connection = conn
+  console.log(conn)
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
@@ -19,20 +20,19 @@ const handleUserInput = function(key){
     process.exit();
   }
   if(key === 'w'){
-    console.log('Move: up')
-    console.log(typeof connection)
+    connection.write('Move: up')
   }
   if(key === 's'){
     console.log('Move: down')
-    //connection.write('Move: down')
+    connection.write('Move: down')
   }
   if(key === 'a'){
     console.log('Move: left')
-    //connection.write('Move: left')
+    connection.write('Move: left')
   }
   if(key === 'd'){
     console.log('Move: right')
-    //connection.write('Move: right')
+    connection.write('Move: right')
   }
   
 }
