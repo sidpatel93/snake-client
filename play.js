8f0978072d756c2445e313fc96387b4ba0d8dc4a
spatel93 <sid.auburn@gmail.com>
2021-11-02 13:56:15 +0000

    separate the client module from play module

--- play.js
+++ play.js
@@ -1,21 +1,8 @@
-const net = require("net");
+
+const {connect} = require('./client')
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "127.0.0.1",
-    port: "50541",
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data)=>{
-    console.log(data)
-  })
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
