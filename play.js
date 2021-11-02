52c040b88381255835142ec4d5550e84b57e8f83
spatel93 <sid.auburn@gmail.com>
2021-11-02 17:12:45 +0000

    implement the key intrupt for client

--- play.js
+++ play.js
@@ -1,8 +1,28 @@
 
+// establishes a connection with the game server
 const {connect} = require('./client')
 
-// establishes a connection with the game server
 
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on('data', handleUserInput)
+
+  return stdin;
+};
+
+const handleUserInput = function(key){
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+
+setupInput();
 console.log("Connecting ...");
 connect();
